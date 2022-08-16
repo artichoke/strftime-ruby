82315ad75ba70c77bea7ee218b58fe19435867f2
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2022-08-16 12:29:28 +0200

    Check time implementation

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -1,6 +1,7 @@
 //! Module containing the formatting logic.
 
 mod assert;
+mod check;
 mod utils;
 mod week;
 mod write;
@@ -9,8 +10,9 @@ use core::fmt;
 use core::num::IntErrorKind;
 use core::str;
 
-use crate::{Error, Time};
+use crate::Error;
 use assert::{assert_sorted, assert_sorted_elem_0, assert_to_ascii_uppercase};
+use check::CheckedTime;
 use utils::{Cursor, SizeLimiter};
 use week::{iso_8601_year_and_week_number, week_number, WeekStart};
 use write::Write;
@@ -368,7 +370,7 @@ impl Piece {
     }
 
     /// Compute UTC offset parts for the `%z` specifier.
-    fn compute_offset_parts(&self, time: &impl Time) -> UtcOffset {
+    fn compute_offset_parts(&self, time: &impl CheckedTime) -> UtcOffset {
         let utc_offset = time.utc_offset();
         let utc_offset_abs = utc_offset.unsigned_abs();
 
@@ -464,7 +466,7 @@ impl Piece {
 
     /// Format time using the formatting directive.
     #[allow(clippy::too_many_lines)]
-    fn fmt(&self, f: &mut SizeLimiter<'_>, time: &impl Time) -> Result<(), Error> {
+    fn fmt(&self, f: &mut SizeLimiter<'_>, time: &impl CheckedTime) -> Result<(), Error> {
         match self.spec {
             Spec::Year4Digits => {
                 let year = time.year();
@@ -473,9 +475,9 @@ impl Piece {
             }
             Spec::YearDiv100 => self.format_num_zeros(f, time.year().div_euclid(100), 2),
             Spec::YearRem100 => self.format_num_zeros(f, time.year().rem_euclid(100), 2),
-            Spec::Month => self.format_num_zeros(f, time.month(), 2),
+            Spec::Month => self.format_num_zeros(f, time.month()?, 2),
             Spec::MonthName => {
-                let index = (time.month() - 1) as usize;
+                let index = (time.month()? - 1) as usize;
                 if self.flags.has_change_or_upper_case() {
                     self.format_string(f, MONTHS_UPPER[index])
                 } else {
@@ -483,25 +485,25 @@ impl Piece {
                 }
             }
             Spec::MonthNameAbbr => {
-                let index = (time.month() - 1) as usize;
+                let index = (time.month()? - 1) as usize;
                 if self.flags.has_change_or_upper_case() {
                     self.format_string(f, &MONTHS_UPPER[index][..3])
                 } else {
                     self.format_string(f, &MONTHS[index][..3])
                 }
             }
-            Spec::MonthDayZero => self.format_num_zeros(f, time.day(), 2),
-            Spec::MonthDaySpace => self.format_num_spaces(f, time.day(), 2),
-            Spec::YearDay => self.format_num_zeros(f, time.day_of_year(), 3),
-            Spec::Hour24hZero => self.format_num_zeros(f, time.hour(), 2),
-            Spec::Hour24hSpace => self.format_num_spaces(f, time.hour(), 2),
+            Spec::MonthDayZero => self.format_num_zeros(f, time.day()?, 2),
+            Spec::MonthDaySpace => self.format_num_spaces(f, time.day()?, 2),
+            Spec::YearDay => self.format_num_zeros(f, time.day_of_year()?, 3),
+            Spec::Hour24hZero => self.format_num_zeros(f, time.hour()?, 2),
+            Spec::Hour24hSpace => self.format_num_spaces(f, time.hour()?, 2),
             Spec::Hour12hZero => {
-                let hour = time.hour() % 12;
+                let hour = time.hour()? % 12;
                 let hour = if hour == 0 { 12 } else { hour };
                 self.format_num_zeros(f, hour, 2)
             }
             Spec::Hour12hSpace => {
-                let hour = time.hour() % 12;
+                let hour = time.hour()? % 12;
                 let hour = if hour == 0 { 12 } else { hour };
                 self.format_num_spaces(f, hour, 2)
             }
@@ -511,7 +513,7 @@ impl Piece {
                 } else {
                     ("am", "pm")
                 };
-                let meridian = if time.hour() < 12 { am } else { pm };
+                let meridian = if time.hour()? < 12 { am } else { pm };
                 self.format_string(f, meridian)
             }
             Spec::MeridianUpper => {
@@ -520,13 +522,13 @@ impl Piece {
                 } else {
                     ("AM", "PM")
                 };
-                let meridian = if time.hour() < 12 { am } else { pm };
+                let meridian = if time.hour()? < 12 { am } else { pm };
                 self.format_string(f, meridian)
             }
-            Spec::Minute => self.format_num_zeros(f, time.minute(), 2),
-            Spec::Second => self.format_num_zeros(f, time.second(), 2),
-            Spec::MilliSecond => self.format_nanoseconds(f, time.nanoseconds(), 3),
-            Spec::FractionalSecond => self.format_nanoseconds(f, time.nanoseconds(), 9),
+            Spec::Minute => self.format_num_zeros(f, time.minute()?, 2),
+            Spec::Second => self.format_num_zeros(f, time.second()?, 2),
+            Spec::MilliSecond => self.format_nanoseconds(f, time.nanoseconds()?, 3),
+            Spec::FractionalSecond => self.format_nanoseconds(f, time.nanoseconds()?, 9),
             Spec::TimeZoneOffsetHourMinute => {
                 self.write_offset_hhmm(f, &self.compute_offset_parts(time))
             }
@@ -548,7 +550,7 @@ impl Piece {
                 }
             }
             Spec::TimeZoneName => {
-                let tz_name = time.time_zone();
+                let tz_name = time.time_zone()?;
                 if !tz_name.is_empty() {
                     assert!(tz_name.is_ascii());
 
@@ -571,7 +573,7 @@ impl Piece {
                 Ok(())
             }
             Spec::WeekDayName => {
-                let index = time.day_of_week() as usize;
+                let index = time.day_of_week()? as usize;
                 if self.flags.has_change_or_upper_case() {
                     self.format_string(f, DAYS_UPPER[index])
                 } else {
@@ -579,7 +581,7 @@ impl Piece {
                 }
             }
             Spec::WeekDayNameAbbr => {
-                let index = time.day_of_week() as usize;
+                let index = time.day_of_week()? as usize;
                 if self.flags.has_change_or_upper_case() {
                     self.format_string(f, &DAYS_UPPER[index][..3])
                 } else {
@@ -587,16 +589,16 @@ impl Piece {
                 }
             }
             Spec::WeekDayFrom1 => {
-                let day_of_week = time.day_of_week();
+                let day_of_week = time.day_of_week()?;
                 let day_of_week = if day_of_week == 0 { 7 } else { day_of_week };
                 self.format_num_zeros(f, day_of_week, 1)
             }
-            Spec::WeekDayFrom0 => self.format_num_zeros(f, time.day_of_week(), 1),
+            Spec::WeekDayFrom0 => self.format_num_zeros(f, time.day_of_week()?, 1),
             Spec::YearIso8601 => {
                 let (iso_year, _) = iso_8601_year_and_week_number(
                     time.year().into(),
-                    time.day_of_week().into(),
-                    time.day_of_year().into(),
+                    time.day_of_week()?.into(),
+                    time.day_of_year()?.into(),
                 );
                 let default_width = if iso_year < 0 { 5 } else { 4 };
                 self.format_num_zeros(f, iso_year, default_width)
@@ -604,31 +606,31 @@ impl Piece {
             Spec::YearIso8601Rem100 => {
                 let (iso_year, _) = iso_8601_year_and_week_number(
                     time.year().into(),
-                    time.day_of_week().into(),
-                    time.day_of_year().into(),
+                    time.day_of_week()?.into(),
+                    time.day_of_year()?.into(),
                 );
                 self.format_num_zeros(f, iso_year.rem_euclid(100), 2)
             }
             Spec::WeekNumberIso8601 => {
                 let (_, iso_week_number) = iso_8601_year_and_week_number(
                     time.year().into(),
-                    time.day_of_week().into(),
-                    time.day_of_year().into(),
+                    time.day_of_week()?.into(),
+                    time.day_of_year()?.into(),
                 );
                 self.format_num_zeros(f, iso_week_number, 2)
             }
             Spec::WeekNumberFromSunday => {
                 let week_number = week_number(
-                    time.day_of_week().into(),
-                    time.day_of_year().into(),
+                    time.day_of_week()?.into(),
+                    time.day_of_year()?.into(),
                     WeekStart::Sunday,
                 );
                 self.format_num_zeros(f, week_number, 2)
             }
             Spec::WeekNumberFromMonday => {
                 let week_number = week_number(
-                    time.day_of_week().into(),
-                    time.day_of_year().into(),
+                    time.day_of_week()?.into(),
+                    time.day_of_year()?.into(),
                     WeekStart::Monday,
                 );
                 self.format_num_zeros(f, week_number, 2)
@@ -651,10 +653,10 @@ impl Piece {
                     (&DAYS, &MONTHS)
                 };
 
-                let week_day_name = &day_names[time.day_of_week() as usize][..3];
-                let month_name = &month_names[(time.month() - 1) as usize][..3];
-                let day = time.day();
-                let (hour, minute, second) = (time.hour(), time.minute(), time.second());
+                let week_day_name = &day_names[time.day_of_week()? as usize][..3];
+                let month_name = &month_names[(time.month()? - 1) as usize][..3];
+                let day = time.day()?;
+                let (hour, minute, second) = (time.hour()?, time.minute()?, time.second()?);
 
                 write!(f, "{week_day_name} {month_name} ")?;
                 write!(f, "{day: >2} {hour:02}:{minute:02}:{second:02} ")?;
@@ -664,8 +666,8 @@ impl Piece {
                 self.write_padding(f, "mm/dd/yy".len())?;
 
                 let year = time.year().rem_euclid(100);
-                let month = time.month();
-                let day = time.day();
+                let month = time.month()?;
+                let day = time.day()?;
 
                 write!(f, "{month:02}/{day:02}/{year:02}")
             }
@@ -677,8 +679,8 @@ impl Piece {
                 let min_width = MIN_WIDTH_NO_YEAR + year_width(year).max(default_year_width);
                 self.write_padding(f, min_width)?;
 
-                let month = time.month();
-                let day = time.day();
+                let month = time.month()?;
+                let day = time.day()?;
 
                 write!(f, "{year:0default_year_width$}-{month:02}-{day:02}")
             }
@@ -686,30 +688,30 @@ impl Piece {
                 let year = time.year();
                 self.write_padding(f, "dd-mmm-".len() + year_width(year).max(4))?;
 
-                let month_name = &MONTHS_UPPER[(time.month() - 1) as usize][..3];
-                let day = time.day();
+                let month_name = &MONTHS_UPPER[(time.month()? - 1) as usize][..3];
+                let day = time.day()?;
 
                 write!(f, "{day: >2}-{month_name}-{year:04}")
             }
             Spec::CombinationTime12h => {
                 self.write_padding(f, "HH:MM:SS PM".len())?;
 
-                let hour = time.hour() % 12;
+                let hour = time.hour()? % 12;
                 let hour = if hour == 0 { 12 } else { hour };
 
-                let (minute, second) = (time.minute(), time.second());
-                let meridian = if time.hour() < 12 { "AM" } else { "PM" };
+                let (minute, second) = (time.minute()?, time.second()?);
+                let meridian = if time.hour()? < 12 { "AM" } else { "PM" };
 
                 write!(f, "{hour:02}:{minute:02}:{second:02} {meridian}")
             }
             Spec::CombinationHourMinute24h => {
                 self.write_padding(f, "HH:MM".len())?;
-                let (hour, minute) = (time.hour(), time.minute());
+                let (hour, minute) = (time.hour()?, time.minute()?);
                 write!(f, "{hour:02}:{minute:02}")
             }
             Spec::CombinationTime24h => {
                 self.write_padding(f, "HH:MM:SS".len())?;
-                let (hour, minute, second) = (time.hour(), time.minute(), time.second());
+                let (hour, minute, second) = (time.hour()?, time.minute()?, time.second()?);
                 write!(f, "{hour:02}:{minute:02}:{second:02}")
             }
         }
@@ -724,7 +726,7 @@ pub(crate) struct TimeFormatter<'t, 'f, T> {
     format: &'f [u8],
 }
 
-impl<'t, 'f, T: Time> TimeFormatter<'t, 'f, T> {
+impl<'t, 'f, T: CheckedTime> TimeFormatter<'t, 'f, T> {
     /// Construct a new `TimeFormatter` wrapper.
     pub(crate) fn new<F: AsRef<[u8]> + ?Sized>(time: &'t T, format: &'f F) -> Self {
         Self {
