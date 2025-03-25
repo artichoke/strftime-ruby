89f54b7428b51d817042f340da57f600e093b68b
Ryan Lopopolo <rjl@hyperbo.la>
2025-03-25 07:31:54 -0700

    Update fuzz/fuzz_targets/string.rs
    Co-authored-by: x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>

--- fuzz/fuzz_targets/string.rs
+++ fuzz/fuzz_targets/string.rs
@@ -30,7 +30,7 @@ impl<'a> fmt::Write for LimitedBuf<'a> {
 
 fuzz_target!(|data: (MockTime, &str)| {
     let (time, format) = data;
-    let _ignored = strftime::string::strftime(&time, format, &mut buf[..]);
+    let _ignored = strftime::string::strftime(&time, format);
 
     // Give each fuzzer input a 16kb buffer to write to.
     let mut buf = vec![0u8; 16 * 1024].into_boxed_slice();
